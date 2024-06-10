ea1c908056cea298f2b3b08fa952e6a4ae8f936b
Roshan Paudel <rozanpoudel@gmail.com>
2024-06-10 21:10:24 +0545

    ✨ NEW: new chanegs

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,21 @@
+import { HandCoins } from "lucide-react";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="flex">
+        <div className="rounded-xl border-rose-100">
+          <div className="bg-primary/10">
+            <HandCoins />
+          </div>
+          <div>
+            <span>Savings</span>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
